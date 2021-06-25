c4d4e89c3e86df4210a21156b3e71b85ab8687ab
Vadim Chernov <dimuls@yandex.ru>
2021-06-25 22:11:53 +0300

    comments

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"mooz/fs"
-	"mooz/ui"
 	"net/http"
 	"os"
 	"os/signal"
@@ -17,53 +15,87 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/websocket/v2"
+
+	"mooz/fs"
+	"mooz/ui"
 )
 
+// Типы сообщений, которые будут передаваться между клиентом и сервером.
 const (
-	ping        = "ping"
-	pong        = "pong"
+	// Ping и pong сообщения необходимы для поддержания websocket соединения.
+	ping = "ping"
+	pong = "pong"
+
+	// Cообщение о готовности websocket соединения.
 	initialized = "initialized"
-	joined      = "joined"
-	left        = "left"
+
+	// Cообщение о подключении нового клиента.
+	joined = "joined"
+
+	// Cообщение об отключении клиента.
+	left = "left"
 )
 
+// Структура стандартного сообщения, которое будет передавться между
+// клиентами и клиентом и сервером.
 type Message struct {
-	To   string      `json:"to,omitempty"`
-	From string      `json:"from,omitempty"`
-	Type string      `json:"type"`
+	// ID клиента, которому направляется сообщение.
+	To string `json:"to,omitempty"`
+	// ID клиента, от которого отправляется сообщение.
+	From string `json:"from,omitempty"`
+	// Тип сообщения.
+	Type string `json:"type"`
+	// Какие-то данные, зависят от типа сообщения.
 	Data interface{} `json:"data,omitempty"`
 }
 
+// Структура клиента.
 type Client struct {
-	id   string
+	// ID клиента.
+	id string
+	// Websocket подключение клиента.
 	conn *websocket.Conn
-	out  chan Message
+	// Канал сообщений, которые будут отправляться клиенту.
+	out chan Message
 }
 
+// Точка входа в сервер
 func main() {
+
+	// Создание fiber приложения.
 	app := fiber.New(fiber.Config{
 		ReadTimeout: 10 * time.Second,
 	})
 
+	// Подключение стандартных middleware.
 	app.Use(recover.New(), logger.New(), cors.New())
 
 	var (
+		// Контекст для завершения работы сервера
 		ctx, cancel = context.WithCancel(context.Background())
-		cs          = map[string]Client{}
-		csMx        sync.RWMutex
+		// Хранилище клиентов.
+		cs = map[string]Client{}
+		// Mutex для конкурентного доступа к хранилищу клиентов.
+		csMx sync.RWMutex
 	)
 
+	// Функция для отправки сообщений сразу всем клиентам.
 	broadcast := func(m Message) {
 		csMx.RLock()
+		// Проходимся по всем клиентам.
 		for id, c := range cs {
+			// Если клиент - это клиент, который отправляет сообщение,
+			// то пропускаем его.
 			if id == m.From {
 				continue
 			}
+			// Направляем сообщение клиенту.
 			c.out <- m
 		}
 		csMx.RUnlock()
 	}
 
+	// Функция для отправки сообщения одному клиенту.
 	send := func(m Message) {
 		csMx.RLock()
 		c, exists := cs[m.To]
@@ -73,29 +105,47 @@ func main() {
 		csMx.RUnlock()
 	}
 
+	// Регистрация websocket-обработчика.
 	app.Get("/ws", websocket.New(func(conn *websocket.Conn) {
 
+		// Тело websocket-обработчика. Тут происходит вся магия по передаче
+		// вспомогательных данных.
+
+		// Создаём клиента.
 		c := Client{
 			id:   fmt.Sprintf("%p", conn),
 			conn: conn,
 			out:  make(chan Message),
 		}
 
+		// Безопасно кладём клиента в хранилище клиентов.
 		csMx.Lock()
 		cs[c.id] = c
 		csMx.Unlock()
 
 		var (
+			// Контекст для завершения websocket-соединения.
 			cCtx, cCancel = context.WithCancel(context.Background())
-			wg            sync.WaitGroup
-			startWg       sync.WaitGroup
 
-			pings   = map[int64]struct{}{}
+			// Wait group для ожидания завершения клиентских обработчиков.
+			wg sync.WaitGroup
+
+			// Wait group для ожидания старта клиентских обработчиков.
+			startWg sync.WaitGroup
+
+			// Хранилище данных для ping-pong. С помощью этих данных
+			// поддерживается устойчивое websocket-соединение.
+			pings = map[int64]struct{}{}
+
+			// Mutex для конкурентного доступа к хранилищу данных ping-pong.
 			pingsMx sync.Mutex
 		)
 
+		// Завершаем контекст websocket соединения при выходе из
+		// websocket-обработчика.
 		defer cCancel()
 
+		// Запуск обработчика сообщений, которые будут отправляться клиенту.
 		wg.Add(1)
 		startWg.Add(1)
 		go func() {
@@ -108,9 +158,11 @@ func main() {
 				select {
 				case <-cCtx.Done():
 					return
+				// Ждём очередное сообщение.
 				case m = <-c.out:
 				}
 
+				// Отправляем сообщение клиенту.
 				err := c.conn.WriteJSON(m)
 				if err != nil {
 					cCancel()
@@ -119,12 +171,14 @@ func main() {
 			}
 		}()
 
+		// Запуск обработчика ping.
 		wg.Add(1)
 		startWg.Add(1)
 		go func() {
 			defer wg.Done()
 			startWg.Done()
 
+			// Создаём тикер с периодом 3 секунды.
 			t := time.NewTicker(3 * time.Second)
 			defer t.Stop()
 
@@ -134,19 +188,26 @@ func main() {
 					return
 				case <-cCtx.Done():
 					return
+				// Раз в 3 секунды
 				case <-t.C:
 				}
 
-				p := time.Now().Unix()
-
 				pingsMx.Lock()
+				// Если в хранилище ping-pong записано больше 5 записей, то
+				// завершаем подключение клиента. Такое может произойти если
+				// соединение не стабильное и часть записей теряется или
+				// не доходят вовремя.
 				if len(pings) > 5 {
 					cCancel()
 					return
 				}
+				// Получаем текущее время в unix timestamp.
+				p := time.Now().Unix()
+				// Записываем в хранилище ping-pong полученное время.
 				pings[p] = struct{}{}
 				pingsMx.Unlock()
 
+				// Отправляем сообщение ping с полученным временем.
 				send(Message{
 					To:   c.id,
 					Type: ping,
@@ -155,6 +216,7 @@ func main() {
 			}
 		}()
 
+		// Запуск обработчика сообщений, которые приходят от клиента.
 		wg.Add(1)
 		startWg.Add(1)
 		go func() {
@@ -172,15 +234,19 @@ func main() {
 				default:
 				}
 
+				// Читаем очередное сообщение.
 				err := c.conn.ReadJSON(&m)
 				if err != nil {
 					cCancel()
 					return
 				}
 
+				// Смотрим тип сообщения.
 				switch m.Type {
 
+				// Если тип сообщения pong.
 				case pong:
+					// Достаём из данных время в unix timestamp.
 					rp, ok := m.Data.(float64)
 					if !ok {
 						cCancel()
@@ -188,13 +254,19 @@ func main() {
 					}
 
 					pingsMx.Lock()
+					// Проходимся по всем записям в хранилище ping-pong.
 					for p := range pings {
-						if p >= int64(rp) {
+						// Если очередная запись, время unix timestamp,
+						// меньше равно полученного времени, то удаляем его из
+						// хранилища.
+						if p <= int64(rp) {
 							delete(pings, p)
 						}
 					}
 					pingsMx.Unlock()
 
+				// Для всех остальных типов сообщений мы отправляем их адресату,
+				// указав клиента-отправителя.
 				default:
 					m.From = c.id
 					send(m)
@@ -202,34 +274,50 @@ func main() {
 			}
 		}()
 
+		// Ожидание старта всех клиентских обработчиков.
 		startWg.Wait()
 
-		broadcast(Message{
+		// Отправляем клиенту сообщение о готовности websocket-соединения.
+		send(Message{
 			Type: initialized,
+			To:   c.id,
 		})
+
+		// Отправляем остальным клиентам сообщение о том что появился новый
+		// клиент.
 		broadcast(Message{
 			Type: joined,
 			From: c.id,
 		})
 
+		// Ожидание окончания контекстов клиента или сервера.
 		select {
 
+		// Если окончился контекст сервера.
 		case <-ctx.Done():
+			// Завершаем контекст клиента.
 			cCancel()
 
+		// Если окончился контекст клиента.
 		case <-cCtx.Done():
+			// Отправляем остальным клиентам сообщение о том, что клиент
+			// вышел из чата.
 			broadcast(Message{
 				Type: left,
 				From: c.id,
 			})
 			csMx.Lock()
+			// Удаляем клиента из хранилища клиентов.
 			delete(cs, c.id)
 			csMx.Unlock()
 		}
 
+		// Ожидание завершения всех клиентских обработчиков.
 		wg.Wait()
 	}))
 
+	// Регистрация обработчики статики (веб-страницы, javascript-файлов и
+	// файлов стилей.)
 	app.Use("/", fs.New(fs.Config{
 		Root:         http.FS(ui.FS),
 		RootPath:     "dist",
@@ -237,24 +325,33 @@ func main() {
 		NotFoundFile: "index.html",
 	}))
 
-	bindAddr := os.Getenv("BIND_ADDR")
+	// Получаем параметр адреса подключения веб-сервера.
+	bindAddr := os.Getenv("LISTEN_ADDR")
 
+	// Если мы будем использовать TLS.
 	if os.Getenv("USE_TLS") == "1" {
 		if bindAddr == "" {
 			bindAddr = ":8443"
 		}
+		// Запускаем HTTPS-сервер.
 		go app.ListenTLS(bindAddr, "cert.pem", "key.pem")
-	} else {
+
+	} else { // Иначе.
 		if bindAddr == "" {
 			bindAddr = ":8080"
 		}
+		// Запускаем HTTP-сервер.
 		go app.Listen(bindAddr)
 	}
 
+	// Ожидание сигнала завершения.
 	exit := make(chan os.Signal)
 	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
 	<-exit
 
+	// Завершаем контекст сервера.
 	cancel()
+
+	// Выключаем сервер.
 	app.Shutdown()
 }
--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,3 +1,4 @@
+<!-- Стили web-страницы -->
 <style>
 html,
 body,
@@ -27,6 +28,7 @@ body,
   top: 0;
   bottom: 0;
 }
+.loading,
 .gesture,
 .error {
   display: flex;
@@ -41,8 +43,9 @@ video {
 }
 </style>
 
+<!-- Шаблон web-страницы -->
 <template>
-  <div class="loading container" v-show="state === 'loading'">загрузка</div>
+  <div class="loading container" v-show="state === 'loading'">Загрузка</div>
   <div class="gesture container" v-show="state === 'gesturing'" ref="gesture">
     Нажмите чтобы начать
   </div>
@@ -55,138 +58,234 @@ video {
   </div>
 </template>
 
+<!-- Javascript web-страницы -->
 <script>
 import { reactive } from "vue";
 
+// Типы сообщений, которые будут передаваться между сервером и клиентом или
+// между клиентами.
+
+// Ping и pong сообщения необходимы для поддержания websocket соединения.
 const ping = "ping";
 const pong = "pong";
+
+// Cообщение о готовности websocket соединения.
 const initialized = "initialized";
+
+// Cообщение о подключении нового клиента.
 const joined = "joined";
+
+// Cообщение об отключении клиента.
 const left = "left";
+
+// Предложение о создании WebRTC-соединения.
 const offer = "offer";
+
+// Ответ на предложение о создании WebRTC-соединения.
 const answer = "answer";
+
+// Информация об ice-кандидате (потенциальном сетевом соединении).
 const iceCandidate = "ice-candidate";
 
+// Состояния web-страницы.
+
+// Загрузка web-страницы.
 const loading = "loading";
+
+// Ожидание начального действия от пользователя
 const gesturing = "gesturing";
+
+// Ошибка web-страницы
 const error = "error";
+
+// Страницы загружена и работает.
 const loaded = "loaded";
 
+// Наше web-приложение.
 export default {
-  name: "App",
+  // Функция data возвращает начальное состояние web-приложения.
   data() {
     return {
+      // Локальные видео и аудио потоки.
       stream: null,
+
+      // Возможная ошибка при работе.
       error: null,
+
+      // Хранилище пиров (WebRTC соединения).
       peers: [],
+
+      // Websocket соединение с сервером.
       ws: {
         connection: null,
-        connected: false,
       },
+
+      // Состояние web-приложения.
       state: loading,
     };
   },
+
+  // Доступные методы.
   methods: {
+    // Создание нового пира с указанным id и флагом "вежливости".
     newPeer(id, bePolite) {
+      // Создание реактивной структуры пира.
       const p = reactive({
+        // ID пира
         id,
+        // Флаг "вежливости"
         bePolite,
+        // WebRTC соединение.
         connection: new RTCPeerConnection(),
+        // Аудио и видео поток пира.
         stream: null,
       });
 
+      // Регистрация обработчика ice-кандидата. Здесь web-браузер передаёт нам
+      // кандидаты на способы подключения с пиром.
       p.connection.onicecandidate = (e) => {
+        // Если нет кандидата, а значит больше их не будет, то выходим из
+        // обработчика.
         if (!e.candidate || !e.candidate.candidate) {
           return;
         }
+        // Отправляем кандидат удалённому пиру.
         this.wsSend(iceCandidate, e.candidate, id);
       };
 
+      // Регистрация обработчика появления нового трека (аудио или видео потока).
+      // Здесь мы получаем аудио и видео поток от удалённого пира.
       p.connection.ontrack = (e) => {
+        // Достаём поток.
         const {
           streams: [stream],
         } = e;
+        // Сохраняем его структуру пира.
         p.stream = stream;
+        // При удалении трека, удаляем его из структуры пира.
         stream.onremovetrack = () => {
           p.stream = null;
         };
       };
 
+      // Добавляем пир в хранилище пиров.
       this.peers.push(p);
 
+      // Возвращаем созданный трек.
       return p;
     },
+    // Получения пира по id.
     getPeer(id) {
       return this.peers.find((p) => p.id === id);
     },
+    // Удаление пира по id.
     removePeer(id) {
       const i = this.peers.findIndex((p) => p.id === id);
+      this.peers[i].close();
       this.peers.splice(i, 1);
     },
+    // Обработчик появления нового клиента (будущий пир).
     async handleJoined(id) {
+      // Создаём новый пир, указываем его ID и устанавливаем "вежливость" в
+      // ложь.
       const p = this.newPeer(id, false);
+      // Получаем треки из аудио и видео потока и добавляем их в соединение
+      // с пиром.
       this.stream
         .getTracks()
         .forEach((t) => p.connection.addTrack(t, this.stream));
+      // Создаём предложение о соединении.
       const o = await p.connection.createOffer();
+      // Сохраняем полученное предложение в соединении с пиром.
       await p.connection.setLocalDescription(o);
+      // Отправляем предложение удалённому пиру.
       this.wsSend(offer, o, id);
     },
+    // Обработчик предложения о соединении.
     async handleOffer(id, offer) {
+      // Пытаемся получить пир, который соответствует отправителю.
       let p = this.getPeer(id);
+      // Флаг того, что предложение от нового пира.
       let newPeer = false;
+      // Если пир не найден, то значит надо его создать и, соответственно
+      // установить флаг нового пира в истину.
       if (!p) {
+        // Создаём новый пир, "вежливость" устанавливаем в истину.
         p = this.newPeer(id, true);
         newPeer = true;
       }
 
+      // Если пир "вежливый", и состояние WebRTC-соединения не стабильное,
+      // то откатываемся к последнему стабильному состоянию.
       if (p.bePolite && p.connection.signalingState !== "stable") {
         await p.connection.setLocalDescription({ type: "rollback" });
       }
+
+      // Сохраняем полученное предложение о соединении.
       await p.connection.setRemoteDescription(offer);
 
+      // Если это был новый пир, то нужно добавить в WebRTC-соединение треки
+      // локальных аудио и видео потоков.
       if (newPeer) {
         this.stream
           .getTracks()
           .forEach((t) => p.connection.addTrack(t, this.stream));
       }
 
+      // Создаём ответ на предложение о соединении.
       const a = await p.connection.createAnswer();
+
+      // Сохраняем созданный ответ.
       await p.connection.setLocalDescription(a);
 
+      // Отправляем ответ удалённому пиру.
       this.wsSend(answer, a, p.id);
     },
+    // Обработчик ответа на предложение о соединении.
     async handleAnswer(id, answer) {
+      // Сохраняем ответ в соответствущем пиру WebRTC-соединении.
       await this.getPeer(id).connection.setRemoteDescription(answer);
     },
+    // Обработчик ice-кандидата
     async handleIceCandidate(id, iceCandidate) {
+      // Добавляем полученный ice-кандидат в WebRTC-соединение, который
+      // соответствует пиру-отправителю.
       await this.getPeer(id).connection.addIceCandidate(iceCandidate);
     },
+    // Метод для отправки сообщений типа type, с данными data и адресатом to.
     wsSend(type, data, to) {
+      // Формируем сообщение нужного типа.
       const msg = { type };
+      // Добавляем данные, если они указаны.
       if (data) {
         msg.data = data;
       }
+      // Добавляем получателя, если он указан.
       if (to) {
         msg.to = to;
       }
+      // Отправляем сообщение на websocket-сервер для дальнейшей маршрутизации.
       this.ws.connection.send(JSON.stringify(msg));
     },
+    // Метод подключения к websocket-серверу.
     wsConnect() {
+      // Создаём соединение на указанный URL сервера.
       this.ws.connection = new WebSocket(this.wsURL);
 
-      this.ws.connection.onopen = () => {
-        this.ws.connected = true;
-      };
-
+      // Обработчик сообщений, которые приходят по websocket-соединению.
       this.ws.connection.onmessage = (e) => {
+        // Декодируем сообщение.
         const msg = JSON.parse(e.data);
 
+        // Если это ping сообщение.
         if (msg.type === ping) {
+          // То отправляем pong полученными данными назад.
           this.wsSend(pong, msg.data);
+          // Выходим из обработчика.
           return;
         }
 
+        // Здесь смотрим тип сообщения и действуем согласно нему.
         switch (msg.type) {
           case initialized:
             this.state = loaded;
@@ -209,21 +308,32 @@ export default {
         }
       };
 
+      // Обработчик закрытия websocket-соединения.
       this.ws.connection.onclose = (e) => {
-        this.ws.connected = false;
+        // Устанавливаем состояние в загрузку.
         this.state = loading;
+        // Отключаемся от всех пиров.
+        this.peers.forEach((p) => p.connection.close());
+        // Удаляем все пиры.
         this.peers.splice(0, this.peers.length);
+        // Если код закрытия соединения не "штатный".
         if (e.code !== 1000) {
+          // То снова подключаемся.
           this.wsConnect();
         }
       };
     },
+    // Метод для запуска ожидания начального действия от пользователя.
     gesture() {
+      // Устанавливаем соответствующее состояние.
       this.state = gesturing;
+      // Возвращаем ожидание на клик пользователем соответствующей области.
       return new Promise((resolve) => {
         this.$refs.gesture.addEventListener("click", resolve, { once: true });
       });
     },
+    // Метод для инициализации аудио и видео потоков из микрофона и камеры
+    // пользователя.
     async initStream() {
       this.stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
@@ -233,17 +343,22 @@ export default {
       });
     },
   },
+  // Обработчик старта нашего web-приложения.
   async mounted() {
+    // Запускаем ожидания начального действия от пользователя.
     await this.gesture();
 
+    // Пытаемся инциализировать аудио и видео потоки пользователя.
     try {
       await this.initStream();
     } catch {
+      // Если не удалось, то меняем состояние web-приложения на ошибку.
       this.state = error;
       this.error = "Не удалось инициализировать микрофон и камеру";
       return;
     }
 
+    // Подключаемся к websocket-серверу.
     this.wsConnect();
   },
 };
